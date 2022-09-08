603877e1b29d19a9be8356d81f26e5fa17faba95
zura-chkhaidze <zura.chkhaidze444@gmail.com>
2022-09-08 13:25:38 +0400

    index.html

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 // burger bar//
 function myFunction() {
-    let x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  let x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+}
+//alert//
+document.getElementById("click me").addEventListener("click", function () {
+  window.alert("Message sent!");
+});
